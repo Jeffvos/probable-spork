2e96f2bf9a7eb56470925a722a34ee763c2994df
Jeff <jeff.vos@aol.com>
2022-10-10 10:10:14 -0400

    adding multiple ip/domain port scan cap

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -2,15 +2,18 @@ import ipaddress
 import socket
 from IPy import IP
 
+def target_scan(target):
+    conv_ip = validate_ip(target)
+
 def scan(ipaddress, port):
     try:
         validated_ip = validate_ip(ipaddress)
         sock = socket.socket()
         sock.settimeout(0.5)
         sock.connect((validated_ip, port))
-        print(f'[+] Port {port} is open')
+        print(f'[+] Port {port} is open - {ipaddress}')
     except:
-        print(f'[-] Failed port {port} closed')
+        print(f'[-] Failed port {port} closed - {ipaddress}')
 
 def validate_ip(ip):
     try:
@@ -21,9 +24,16 @@ def validate_ip(ip):
 
 
 if __name__ == "__main__":
-    ipaddress = input('[+] Enter target ip ')
-    port_start = int(input('[+] Start of the port range '))
-    port_end = int(input('[+] End of the port range ')) + 1
-    for port in range(port_start, port_end):
-        scan(ipaddress=ipaddress, port=port)
+    target = input('[+] Enter target(s) ip, (use , to specify multiple targets): ')
+    port_start = int(input('[+] Start of the port range: '))
+    port_end = int(input('[+] End of the port range: ')) + 1
+    if ',' in target:
+        for ipadd in target.split(','):
+            for port in range(port_start, port_end):
+                scan(ipaddress=ipadd.strip(' '), port=port)
+    else:
+        for port in range(port_start, port_end):
+            scan(ipaddress=target, port=port)
+
+    
 
