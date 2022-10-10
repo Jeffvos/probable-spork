1e6caa7afe539c3b7c091cd60d3cec36f81a428f
Jeff <jeff.vos@aol.com>
2022-10-10 10:50:58 -0400

    init

--- vulscan/main.py
+++ vulscan/main.py
@@ -3,3 +3,5 @@ from ..portscan import portscan
 targets = input('[+] specify target to scan: ')
 ports = int(input('[+] enter the max ports to scan: '))
 file_to_vul = input('[+] specify path to vulnerabilities file: ')
+
+portscan.scan(targets)
