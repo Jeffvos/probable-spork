88673ed28bcc1b31f4efa759a3532c6fa20de231
Jeff <jeff.vos@aol.com>
2022-10-10 10:41:52 -0400

    beginning of vulnerabilities scanner

---
+++ vulscan/main.py
@@ -0,0 +1,5 @@
+from ..portscan import portscan
+
+targets = input('[+] specify target to scan: ')
+ports = int(input('[+] enter the max ports to scan: '))
+file_to_vul = input('[+] specify path to vulnerabilities file: ')
