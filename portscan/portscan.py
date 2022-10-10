90151019bd8698d6ce0cd6152e66184ab22bff68
Jeff <jeff.vos@aol.com>
2022-10-10 09:32:28 -0400

    adding a simple port scan tool adding range of ports

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -2,12 +2,18 @@ import ipaddress
 import socket
 from IPy import IP
 
-ipaddress = input('[+] Enter the ip ')
-port = int(input('[+] Port '))
+def scan(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print(f'[+] Port {port} is open')
+    except:
+        print(f'[+] Failed port {port} closed')
 
-try:
-    sock = socket.socket()
-    sock.connect((ipaddress, port))
-    print(f'[+] Port {port} is open')
-except:
-    print(f'[+] Failed port {port} closed')
+if __name__ == "__main__":
+    ipaddress = input('[+] Enter target ip ')
+    port_start = int(input('[+] Start of the port range '))
+    port_end = int(input('[+] End of the port range '))
+    for port in range(port_start, port_end):
+        scan(ipaddress=ipaddress, port=port)
+
