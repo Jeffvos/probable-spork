a6aeaabfd93304ca1031ad826391e2747bb9f9b2
jeffvos <jeff.vos@aol.com>
2022-11-08 10:14:07 +0100

    adding all to class

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -2,47 +2,37 @@ import ipaddress
 import socket
 from IPy import IP
 
-def target_scan(target):
-    conv_ip = validate_ip(target)
 
-def scan(ipaddress, port):
-    try:
-        validated_ip = validate_ip(ipaddress)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        sock.connect((validated_ip, port))
+class PortScan:
+    def __init__(self, target, port):
+        self.target = target
+        self.port = port
+
+
+    def target_scan(self):
+        conv_ip = validate_ip(self.target)
+
+    def scan(self):
         try:
-            banner = get_banner(sock)
-            print(f'[+] Port {port} is open - {ipaddress} - {banner}')
+            validated_ip = validate_ip(self.target)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.5)
+            sock.connect((validated_ip, self.port))
+            try:
+                banner = get_banner(sock)
+                print(f'[+] Port {self.port} is open - {self.target} - {banner}')
+            except:
+                print(f'[+] Port {self.port} is open - {self.target}')
         except:
-            print(f'[+] Port {port} is open - {ipaddress}')
-    except:
-        print(f'[-] Failed port {port} closed - {ipaddress}')
+            print(f'[-] Failed port {self.port} closed - {self.target}')
 
-def get_banner(so):
-    print(f'so {so.recv(1024).decode()}')
-    return so.recv(1024).decode()
+    def get_banner(self, so):
+        print(f'so {so.recv(1024).decode()}')
+        return so.recv(1024).decode()
 
-def validate_ip(ip):
-    try:
-        IP(ip)
-        return ip
-    except ValueError:
-        return socket.gethostbyname(ip)
-
-
-if __name__ == "__main__":
-    target = input('[+] Enter target(s) ip, (use , to specify multiple targets): ')
-    port_start = int(input('[+] Start of the port range: '))
-    port_end = int(input('[+] End of the port range: ')) + 1
-    if ',' in target:
-        for ipadd in target.split(','):
-            print("\n")
-            for port in range(port_start, port_end):
-                scan(ipaddress=ipadd.strip(' '), port=port)
-    else:
-        for port in range(port_start, port_end):
-            scan(ipaddress=target, port=port)
-
-    
-
+    def validate_ip(self, ip):
+        try:
+            IP(ip)
+            return ip
+        except ValueError:
+            return socket.gethostbyname(ip)
--- vulscan/main.py
+++ vulscan/main.py
@@ -3,5 +3,6 @@ from ..portscan import portscan
 targets = input('[+] specify target to scan: ')
 ports = int(input('[+] enter the max ports to scan: '))
 file_to_vul = input('[+] specify path to vulnerabilities file: ')
+print('\n')
 
 portscan.scan(targets)
